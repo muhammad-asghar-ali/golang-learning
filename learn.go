a86d9773fd691989fe1317cfb95e100a9c3e329b
unknown <alikhan137>
2022-09-20 23:53:21 -0700

    learning basics of golang programming language

--- learn.go
+++ learn.go
@@ -1,11 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	///////////////////////////////// Variables & Data Types
+	///////////////////////////////// Variables & Data Types ///////////////////////////////////////
 	// var s string = "String"
 	// var number uint = 260
 	// // overflow the number
@@ -27,7 +25,7 @@ func main() {
 
 	// fmt.Println("Hello World")
 
-	///////////////////////////////// Assignment Expression & Implicit vs Explicit
+	///////////////////////////////// Assignment Expression & Implicit vs Explicit ///////////////////////////////
 	// // explicit define
 
 	// var ex uint = 16
@@ -41,7 +39,7 @@ func main() {
 	// fmt.Println(ex)
 	// fmt.Printf("%T", imp)
 
-	////////////////////////////////////// Printing to Console & fmt
+	////////////////////////////////////// Printing to Console & fmt //////////////////////////////////////////
 	// var nu uint64
 	// var bl bool
 	// var st string
@@ -86,29 +84,100 @@ func main() {
 	// var integer2 = 23
 	// fmt.Printf("hello %T %05d \n", integer2, integer2) // hello int 00023
 
-	//////////////////////////////////// Sprint
+	//////////////////////////////////// Sprint ////////////////////////////////////////
 
 	// var out string = fmt.Sprintf("hello %T %05d \n", 34, 34)
 	// print(out)
 
-	////////////////////////////////////// Console Input (Bufio Scanner) & Type Conversion
+	////////////////////////////////////// Console Input (Bufio Scanner) & Type Conversion /////////////////////////////////
 	// scanner := bufio.NewScanner(os.Stdin)
 	// fmt.Printf("Type the year you were born: ")
 	// scanner.Scan()
 	// input, _ := strconv.ParseInt(scanner.Text(), 10, 64)
 	// fmt.Printf("you age are %d years old.", 2022-input)
 
-	//////////////////////////////////// Arithmetic Operators & Math
-	var num1 float64 = 9
-	var num2 float64 = 4
-	sum := num1 + num2
-	diff := num1 - num2
-	mul := num1 * num2
-	div := num1 / num2
+	//////////////////////////////////// Arithmetic Operators  /////////////////////////////////////////
+	// var num1 int = 9
+	// var num2 int = 4
+	// sum := num1 + num2
+	// diff := num1 - num2
+	// mul := num1 * num2
+	// div := num1 / num2
+	// mod := num1 % num2
+	// m := 2*(num1*num2) - num1/2
 
-	fmt.Printf("Sum: %g \n", sum)
-	fmt.Printf("Diff: %g \n", diff)
-	fmt.Printf("Mul: %g \n", mul)
-	fmt.Printf("Div: %g \n", div) // 2.5
+	// fmt.Printf("Sum: %d \n", sum)
+	// fmt.Printf("Diff: %d \n", diff)
+	// fmt.Printf("Mul: %d \n", mul)
+	// fmt.Printf("Div: %d \n", div) // 2
+	// fmt.Printf("Mod: %d \n", mod)
+	// fmt.Printf("Arithmetic expression: %d \n", m)
 
+	////////////////////////////////////////// Conditions & Boolean Expressions ////////////////////////////////////
+	// a := 6
+	// b := 7
+	// fmt.Printf("%t \n", a < b)  // true
+	// fmt.Printf("%t \n", a > b)  // false
+	// fmt.Printf("%t \n", a == b) // false
+	// fmt.Printf("%t \n", a <= b) // true
+	// fmt.Printf("%t \n", a >= b) // false
+	// fmt.Printf("%t \n", a != b) // true
+
+	///////////////////////////////////////// Chained Conditionals (AND, OR, NOT) ////////////////////////////////
+
+	// x := 4
+	// y := 9
+
+	// fmt.Printf("%t \n", x < y || x > y)   //true
+	// fmt.Printf("%t \n", x < y && x > y)   // false
+	// fmt.Printf("%t \n", x == y || x == y) // false
+	// fmt.Printf("%t \n", x == y && x == y) // false
+	// fmt.Printf("%t \n", x <= y || x >= y) // true
+	// fmt.Printf("%t \n", x <= y && x >= y) // false
+	// fmt.Printf("%t \n", x != y && x != y) // true
+	// fmt.Printf("%t \n", x != y || x != y) // true
+
+	// fmt.Printf("%t \n", false || true && false)    // false
+	// fmt.Printf("%t \n", (false || true) && false)  // false
+	// fmt.Printf("%t \n", false || !(true && false)) // true
+
+	// fmt.Printf("%t \n", true || false && true)    // true
+	// fmt.Printf("%t \n", true || (false && true))  // true
+	// fmt.Printf("%t \n", true || !(false && true)) // true
+
+	///////////////////////////////////////////////// If, Else If, Else ////////////////////////////////////////////
+
+	// age := 16
+	// if age < 17 {
+	// 	fmt.Println("you can not ride")
+	// 	fmt.Printf("wait %d years to ride", 18-age)
+	// } else if age >= 14 && age <= 17 {
+	// 	fmt.Println("you can ride with parent")
+	// } else {
+	// 	fmt.Println("you can ride")
+	// }
+
+	///////////////////////////////////////////////// For Loops & While Loops ////////////////////////////////////
+
+	// for loop
+	// x := 3
+	// for x < 5 {
+	// 	fmt.Println(x)
+	// 	x++
+	// }
+
+	// for y := 0; y <= 5; y++ {
+	// 	fmt.Println(y)
+	// }
+
+	for y := 0; y <= 1000; y++ {
+		if y != 0 && y%3 == 0 && y%7 == 0 && y%9 == 0 {
+			fmt.Println(y)
+			continue
+			// break
+		}
+		// else {
+		// 	fmt.Println("N")
+		// }
+	}
 }
